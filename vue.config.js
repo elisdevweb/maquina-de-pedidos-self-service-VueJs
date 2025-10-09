a5db23ef3f17d43d9da03d2e5f5e26c733d1c8c0
elisa <elisabeth.silva@portodesantos.gov.br>
2025-10-09 11:44:45 -0300

    salvando alterações antes do deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/maquina-de-pedidos-self-service-VueJs/"
+      : "/",
 });
